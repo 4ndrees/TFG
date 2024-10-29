f2562c49a9572a1b3e059e77a220e23abcd12f65
Andres Viñe <vs1andres@gmail.com>
2024-10-29 10:08:25 +0100

    Add all ResNet Classificator code

---
+++ ResNet/Data_Paths.py
@@ -0,0 +1,17 @@
+# ------------------------------------------------------#
+
+# Rutas de datos específicas de nuestro ordenador.
+#Cambiar la ruta dependiendo de donde tengas los datos.
+
+# ------------------------------------------------------#
+
+# Dataset
+TRAIN_DATASET = r"C:\\Users\\vssta\\OneDrive\\Escritorio\\pruebas_csv_imagenes\\images\\train"
+TEST_DATASET = r"C:\\Users\\vssta\\OneDrive\\Escritorio\\pruebas_csv_imagenes\\images\\test"
+VAL_DATASET = r"C:\\Users\\vssta\\OneDrive\\Escritorio\\pruebas_csv_imagenes\\images\\valid"
+
+# CSV
+CSV_DATSET = r"C:\\Users\\vssta\\OneDrive\\Escritorio\\pruebas_csv_imagenes\\Dataset.csv"
+
+# MODEL
+MODEL_DATASET = "C:\\Users\\vssta\\OneDrive\\Escritorio\\pruebas_csv_imagenes\\model"
---
+++ ResNet/README.md
@@ -0,0 +1,7 @@
+Para utilizar el siguiente codigo teneis que seguir los siguientes pasos:
+
+1. Cambiar en Data_Paths.py las rutas a las carpetas en las que se almacena el dataset y el modelo
+2. Si no habeis descargado las imagenes, desde Image_Loader.ipynb hay un código para descargar el dataset desde Kaggle
+3. Podeis ejecutar el código en Label_Dataset.ipynb si necesitais un csv que etiquete todos los datos (no es necesario en principio)
+4. El archivo Data_Preprocessing.ipynb tampoco es necesario de momento pero si queremos hacer un preprocesado de imagenes más concreto lo hacemos en ese archivo
+5. Lo último es utilizar el archivo ResNet.ipynb que creo que está bien expicado
